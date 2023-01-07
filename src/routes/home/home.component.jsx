d199a9202f7a25cd7668c7d264bdf974d8b076f9
Guang Yang <gyang55@my.bcit.ca>
2023-01-07 15:52:25 -0800

    change to official codes

--- src/routes/home/home.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import Directory from "../../component/directory/directory.component";
-const Home = () => {
-  return <Directory></Directory>;
-};
-export default Home;
